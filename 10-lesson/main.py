2f216b8dae9de7ef6d38a169152cc25bb179cbba
AdmBeck <admbeck@outlook.com>
2022-11-17 21:17:57 +0500

    lesson 10

---
+++ 10-lesson/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# list1 = [i for i in range(10)]
+
+# def mult(val):
+#     if val % 2:
+#         return val * 3
+#     else:
+#         return val * 5
+
+# print(list(map(mult, list1)))
+
+
+# list_word = ["linux", "python", "windows", "micros"]
+# def separate(word):
+#     return list(word)
+
+# print(list(map(separate, list_word)))
+
+# print(list(map(list, list_word)))
