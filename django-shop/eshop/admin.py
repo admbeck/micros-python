6657a68d9bfb1765c2e6596afe348fa33e62949e
AdmBeck <admbeck@outlook.com>
2023-02-14 21:16:21 +0500

    progress during lesson

--- django-shop/eshop/admin.py
+++ django-shop/eshop/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Product, Category, Gallery
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+class GalleryInline(admin.TabularInline):
+    """Creates inline image upload in admin panel"""
+    fk_name = 'product'
+    model = Gallery
+    extra = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Creates automatic slugs"""
+    list_display = ('title', 'parent', 'get_products_count')
+    prepopulated_fields = {'slug': ('title',)}
+
+    def get_products_count(self, obj):
+        """Gets sum total of available produtcs for admin page"""
+        if obj.products:
+            return len(obj.products.all())
+        else:
+            return 0
+
+    get_products_count.short_description = 'Product quantity'
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Display products in admin panel"""
+    list_display = ('pk', 'title', 'category', 'quantity', 'price', 'created_at', 'size', 'color', 'get_thumbnail')
+    list_editable = ('price', 'quantity', 'size', 'color')
+    list_display_links = ('pk', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('title', 'price')
+    inlines = (GalleryInline,)
+
+    def get_thumbnail(self,obj):
+        """Creates thumbnail for admin page"""
+        if obj.images:
+            return mark_safe(f'<img src="{obj.images.all()[0].image.url}" width="75">')
+        else:
+            return '-'
+
+    get_thumbnail.short_description = 'Thumbnail'
+
+admin.site.register(Gallery)
--- django-shop/eshop/models.py
+++ django-shop/eshop/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+# from colorama.ansi import clear_screen
 
-# Create your models here.
+class Category(models.Model):
+    """Creates table with categories for shop inventory"""
+    title = models.CharField(max_length=150, verbose_name='Category name')
+    image = models.ImageField(upload_to='categories/', null=True, blank=True, verbose_name='Images')
+    slug = models.SlugField(unique=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Sub-category', related_name='subcategories')
+
+    def get_absolute_url(self):
+        pass
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'Category: pk={self.pk}, title={self.title}'
+
+    class Meta:
+        """Used for translation"""
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Product(models.Model):
+    """Creates table of product items"""
+    title = models.CharField(max_length=255, verbose_name='Product name')
+    price = models.FloatField(verbose_name='Price')
+    watched = models.IntegerField(default=0, verbose_name='Views')
+    quantity = models.IntegerField(default=0, verbose_name='Quantity of items')
+    description = models.TextField(default='Product description', verbose_name='Product description')
+    info = models.TextField(default='Information on the product', verbose_name='Product info')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Date of creation')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Categories', related_name='products')
+    slug = models.SlugField(unique=True, null=True)
+    size = models.IntegerField(default=30,verbose_name='Size in mm')
+    color = models.CharField(max_length=30, default='Template color', verbose_name='Color/Material')
+
+    def get_absolute_url(self):
+        pass
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'Product: pk={self.pk}, title={self.title}, price={self.price}'
+
+    class Meta:
+        """Used for translation"""
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+class Gallery(models.Model):
+    """Creates table for images"""
+    image = models.ImageField(upload_to='products/', verbose_name='Image')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+
+    class Meta:
+        """Used for translation"""
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
